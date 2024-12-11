35349018d8cbe6e6d46631a016143a5b1027ee6b
Anonymous-Exam-User <191143555+Anonymous-Exam-User@users.noreply.github.com>
2024-12-11 17:01:53 +0000

    Fixinf conflict with select() between mass and dplyr packages

--- functions/cleaning.R
+++ functions/cleaning.R
@@ -1,22 +1,48 @@
+cleaning_columns <- function(raw_data){
+  raw_data %>% 
+    dplyr::select(-Comments) %>% #later use of MASS package causes conflict. 
+    dplyr::select(-starts_with ("Delta")) %>%  
+    clean_names()
+}
+
+
+## ---------------------------
+##
+## Script name: Cleaning.r
+##
+## Purpose of script: 
+##      # A file of functions for cleaning the Palmer Penguins dataset
+##
+## Author: Dr. Lydia France
+##
+## Date Created: 2024-10-01 
+##
+##
+## ---------------------------
+##
+## Notes:
+##   
+##
+## ---------------------------
+
+
 
 # A function to make sure the column names are cleaned up, 
 # eg lower case and snake case
-cleaning_columns <- function(raw_data){
-  raw_data %>% 
-    select(-Comments) %>% 
-    select(-starts_with ("Delta")) %>%
+clean_column_names <- function(penguins_data) {
+  penguins_data %>%
     clean_names()
 }
 
 # A function to remove columns based on a vector of column names
-remove_columns <- function(penguins_raw, column_names) {
-  penguins_raw %>%
-    select(-starts_with(column_names))
+remove_columns <- function(penguins_data, column_names) {
+  penguins_data %>%
+    dplyr::select(-starts_with(column_names))
 }
 
 # A function to make sure the species names are shortened
-shorten_species <- function(penguins_raw) {
-  penguins_raw %>%
+shorten_species <- function(penguins_data) {
+  penguins_data %>%
     mutate(species = case_when(
       species == "Adelie Penguin (Pygoscelis adeliae)" ~ "Adelie",
       species == "Chinstrap penguin (Pygoscelis antarctica)" ~ "Chinstrap",
@@ -25,22 +51,14 @@ shorten_species <- function(penguins_raw) {
 }
 
 # A function to remove any empty columns or rows
-remove_empty_columns_rows <- function(penguins_raw) {
-  penguins_raw %>%
+remove_empty_columns_rows <- function(penguins_data) {
+  penguins_data %>%
     remove_empty(c("rows", "cols"))
 }
 
 
 # A function to remove rows which contain NA values
-remove_NA <- function(penguins_raw) {
-  penguins_raw %>%
+remove_NA <- function(penguins_data) {
+  penguins_data %>%
     na.omit()
 }
-
-cleaning_columns_geographical <- function(raw_data){
-  raw_data %>% 
-    select(-Comments) %>% 
-    clean_names() 
-  
-}
-
