250aa5ad4f86de7e3083a14366d974087422518e
Anonymous-Exam-User <191143555+Anonymous-Exam-User@users.noreply.github.com>
2024-12-12 02:44:30 +0000

    added print() to funcitons.

--- functions/cleaning.R
+++ functions/cleaning.R
@@ -2,7 +2,9 @@ cleaning_columns <- function(raw_data){
   raw_data %>% 
     dplyr::select(-Comments) %>% #later use of MASS package causes conflict. 
     dplyr::select(-starts_with ("Delta")) %>%  
-    clean_names()
+    clean_names() %>% 
+    print("Columns names have been cleaned with Delta and comment columns removed")
+  
 }
 
 
@@ -31,13 +33,18 @@ cleaning_columns <- function(raw_data){
 # eg lower case and snake case
 clean_column_names <- function(penguins_data) {
   penguins_data %>%
-    clean_names()
+    clean_names() %>% 
+    print("Columns names have been cleaned ")
+  
+  
 }
 
 # A function to remove columns based on a vector of column names
 remove_columns <- function(penguins_data, column_names) {
   penguins_data %>%
-    dplyr::select(-starts_with(column_names))
+    dplyr::select(-starts_with(column_names)) %>% 
+    print("Specified columns have been removed")
+  
 }
 
 # A function to make sure the species names are shortened
@@ -47,18 +54,24 @@ shorten_species <- function(penguins_data) {
       species == "Adelie Penguin (Pygoscelis adeliae)" ~ "Adelie",
       species == "Chinstrap penguin (Pygoscelis antarctica)" ~ "Chinstrap",
       species == "Gentoo penguin (Pygoscelis papua)" ~ "Gentoo"
-    ))
+    )) %>% 
+    print("Species names have been shortened")
+  
 }
 
 # A function to remove any empty columns or rows
 remove_empty_columns_rows <- function(penguins_data) {
   penguins_data %>%
-    remove_empty(c("rows", "cols"))
+    remove_empty(c("rows", "cols")) %>% 
+    print("Columns and Rows which were empty have been removed")
+  
+  
 }
 
 
 # A function to remove rows which contain NA values
 remove_NA <- function(penguins_data) {
   penguins_data %>%
-    na.omit()
+    na.omit() %>% 
+    print("Rows containing NA values have been removed")
 }
